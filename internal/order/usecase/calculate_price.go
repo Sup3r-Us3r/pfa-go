3eb15b8ddd55df3e79b2845d713151620247b17a
Sup3r-Us3r <maydersonmello@gmail.com>
2022-10-01 13:57:46 -0300

    create calculate price use case

---
+++ internal/order/usecase/calculate_price.go
@@ -0,0 +1,51 @@
+package usecase
+
+import "github.com/Sup3r-Us3r/pfa-go/internal/order/entity"
+
+type OrderInputDTO struct {
+	Id    string
+	Price float64
+	Tax   float64
+}
+
+type OrderOutputDTO struct {
+	Id         string
+	Price      float64
+	Tax        float64
+	FinalPrice float64
+}
+
+type CalculateFinalPriceUseCase struct {
+	OrderRepository entity.OrderRepositoryInterface
+}
+
+func NewCalculateFinalPriceUseCase(orderRepository entity.OrderRepositoryInterface) *CalculateFinalPriceUseCase {
+	return &CalculateFinalPriceUseCase{OrderRepository: orderRepository}
+}
+
+func (c *CalculateFinalPriceUseCase) Execute(input OrderInputDTO) (*OrderOutputDTO, error) {
+	order, err := entity.NewOrder(input.Id, input.Price, input.Tax)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = order.CalculateFinalPrice()
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.OrderRepository.Save(order)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &OrderOutputDTO{
+		Id:         order.Id,
+		Price:      order.Price,
+		Tax:        order.Tax,
+		FinalPrice: order.FinalPrice,
+	}, nil
+}
