b1d643b45aff0a39fc571a719e7081ee1aa43d99
Sup3r-Us3r <maydersonmello@gmail.com>
2022-10-01 13:57:59 -0300

    create get total use case

---
+++ internal/order/usecase/get_total.go
@@ -0,0 +1,25 @@
+package usecase
+
+import "github.com/Sup3r-Us3r/pfa-go/internal/order/entity"
+
+type GetTotalOutputDTO struct {
+	Total int `json:"total"`
+}
+
+type GetTotalUseCase struct {
+	OrderRepository entity.OrderRepositoryInterface
+}
+
+func NewGetTotalUseCase(orderRepository entity.OrderRepositoryInterface) *GetTotalUseCase {
+	return &GetTotalUseCase{OrderRepository: orderRepository}
+}
+
+func (o *GetTotalUseCase) Execute() (*GetTotalOutputDTO, error) {
+	totalOrders, err := o.OrderRepository.GetTotal()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &GetTotalOutputDTO{Total: totalOrders}, nil
+}
