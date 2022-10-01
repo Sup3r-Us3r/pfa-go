629d0e93e04be1ec4221c2dfaeda3a38ee5fc657
Sup3r-Us3r <maydersonmello@gmail.com>
2022-10-01 13:55:34 -0300

    create order entity

---
+++ internal/order/entity/order.go
@@ -0,0 +1,59 @@
+package entity
+
+import "errors"
+
+type Order struct {
+	Id         string
+	Price      float64
+	Tax        float64
+	FinalPrice float64
+}
+
+type OrderRepositoryInterface interface {
+	Save(order *Order) error
+	GetTotal() (int, error)
+}
+
+func (o Order) IsValid() error {
+	if o.Id == "" {
+		return errors.New("invalid id")
+	}
+
+	if o.Price == 0 {
+		return errors.New("invalid price")
+	}
+
+	if o.Tax == 0 {
+		return errors.New("invalid tax")
+	}
+
+	return nil
+}
+
+func (o *Order) CalculateFinalPrice() error {
+	o.FinalPrice = o.Price + o.Tax
+
+	err := o.IsValid()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewOrder(id string, price float64, tax float64) (*Order, error) {
+	order := &Order{
+		Id:    id,
+		Price: price,
+		Tax:   tax,
+	}
+
+	err := order.IsValid()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return order, nil
+}
